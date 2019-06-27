f2f3de76f0ba3b7a20eca000cd34fadc091b43ad
Andrej T <andrejt@hfour.com>
2019-06-27 13:47:44 +0200

    limited time per move; added events

--- src/server.ts
+++ src/server.ts
@@ -8,13 +8,19 @@ let app = express();
 
 interface Game {
     id: string;
-    status: 'created';
+    status: 'created' | 'ready' | 'started'; // ready when 2+ players; started after first move
     createdAt: Date;
     playerIds: string[];
-    moves: string[];
+    moves: { timestamp: Date, move: string }[];
+    events: string[]
 }
 
 const GAMES_DATA_LOCATION = path.join(__dirname, '../data/games.json'); // up, out of the build folder
+const TIME_PER_MOVE = 30;
+
+function timeDiffSeconds(t1: Date, t2: Date) {
+    return (t2.getTime() - t1.getTime()) / 1000;
+}
 
 class Games {
     games: Game[];
@@ -35,7 +41,9 @@ class Games {
 
     create() {
         let id = uuid();
-        this.games.push({ id, status: 'created', playerIds: [], createdAt: new Date(), moves: [] })
+        let game: Game = { id, status: 'created', playerIds: [], createdAt: new Date(), moves: [], events:[] };
+        game.events.push('Game created.');
+        this.games.push(game)
         this.save();
         return id;
     }
@@ -48,16 +56,43 @@ class Games {
         return game;
     }
 
-    joinGame(gameId: string, playerId: string) {
+    private getSecondsSinceLastMove(gameId: string) {
         let game = this.findGame(gameId);
+        if (!game.moves.length) {
+            return 0;
+        }
+        let lastMoveTimestamp = game.moves[game.moves.length - 1].timestamp;
+        return ((new Date()).getTime() - lastMoveTimestamp.getTime()) / 1000;
+    }
+
+    private ensureGameNotStale(gameId: string) {
+        if (this.getSecondsSinceLastMove(gameId) > TIME_PER_MOVE) {
+            throw new Error('Too much time has passed since the last move; game will automatically end soon.')
+        }
+    }
+
+    joinGame(gameId: string, playerId: string) {
+        this.ensureGameNotStale(gameId);
+        let game = this.findGame(gameId);
+        if (game.playerIds.length === 4) {
+            throw new Error('Game is full.');
+        }
         game.playerIds.push(playerId);
+        game.status = 'ready';
+        game.events.push(`Player "${playerId}" joined game.`);
         this.save();
         return game;
     }
 
     makeMove(gameId: string, playerId: string, move: string) {
+        this.ensureGameNotStale(gameId);
         let game = this.findGame(gameId);
-        game.moves.push(move);
+        if (game.status === 'created') {
+            throw new Error('Game is not ready yet.')
+        }
+        if (game.status === 'ready') game.status = 'started';
+        game.moves.push({ timestamp: new Date(), move });
+        game.events.push(`Player "${playerId}" played move "${move}".`);
         this.save();
         return game;
     }
